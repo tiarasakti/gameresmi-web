cfea3d7c4a19e4ed5b1ff0f559e0b23e4a220582
tiarasakti <167270306+tiarasakti@users.noreply.github.com>
2025-12-18 02:24:24 +0700

    Delete functions/api directory

--- functions/api/ping.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest({ env }) {
-  return new Response(JSON.stringify({
-    ok: true,
-    hasAdminToken: !!env.ADMIN_TOKEN,
-    hasDB: !!env.DB,
-    hasFILES: !!env.FILES,
-  }), { headers: { "content-type": "application/json; charset=utf-8" }});
-}
