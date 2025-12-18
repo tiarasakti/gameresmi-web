8f4becbee56d373e480dcb90e1be7dcf4e5258a5
tiarasakti <167270306+tiarasakti@users.noreply.github.com>
2025-12-19 00:16:18 +0700

    Delete functions/api directory

--- functions/api/admin/games-list.js
+++
@@ -1,7 +0,0 @@
-export { onRequestGet } from "./games.js";
-export async function onRequest() {
-  return new Response(JSON.stringify({ error: "Method not allowed" }), {
-    status: 405,
-    headers: { "content-type": "application/json; charset=utf-8" }
-  });
-}
--- functions/api/ping.js
+++
@@ -1,5 +0,0 @@
-export async function onRequestGet() {
-  return new Response(JSON.stringify({ ok: true, ts: Date.now() }), {
-    headers: { "content-type": "application/json; charset=utf-8" },
-  });
-}
